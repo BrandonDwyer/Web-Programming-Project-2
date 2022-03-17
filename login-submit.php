08e1d0e7025128545538685cd44c1b1a693e4dba
oxlngo <96035405+oxlngo@users.noreply.github.com>
2022-03-17 19:08:27 -0400

    secon commit

---
+++ login-submit.php
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+    <head>
+        <title>Jeopardy! - Login</title>
+        <link type = "text/css" rel = "stylesheet" href = "styles.css"/>
+        <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+    </head>
+    <body>
+        <?php
+    session_start();
+    $csv_file = file_get_contents('users.txt');
+    $file = fopen("php://temp", 'r+');
+    fputs($file,$csv_file);
+    rewind($file);
+    $users = [];
+    while(($data = fgetcsv($file)) !== FALSE){
+        $users[] = $data;
+    }
+    $loginFailed = TRUE;
+
+    for($i = 0; $i < count($users); $i++){
+        if(!isset($users[$i][0])){ //Error handling in case of bad pushes to the csv file.
+            continue;
+        }
+        else{
+            if($_POST['Username'] == $users[$i][0] && $_POST['Password'] == $users[$i][1]){
+                $_SESSION['Name'] = $users[$i][2];
+                $loginFailed = FALSE;
+            }
+        }
+    }
+    print_r($users);
+    if($loginFailed == TRUE){
+        $_SESSION['loginFail'] = TRUE;
+        header("Location: index.php");
+    }
+    else{
+        header("Location: main.php");
+    }
+        ?>
+    </body>
+</html>
