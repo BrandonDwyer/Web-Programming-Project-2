f1b49d0203680d04528bfc1bedb8e3f642d856c3
oxlngo <96035405+oxlngo@users.noreply.github.com>
2022-03-17 19:14:32 -0400

    seon commit

--- signup.php
+++ signup.php
@@ -8,18 +8,29 @@
         <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
     </head>
     <body>
-    
-    
+       
+    <?php
+            session_start();
+        ?>
+        
+        <?php
+            if(isset($_SESSION['signupFail'])){
+                
+                if($_SESSION['signupFail'] == TRUE){
+                    echo "<h2>User already used. Please try again.</h2>";
+                }
+            }
+        ?>
         <form action = "signup-submit.php" method = "POST">
             <fieldset>
                 <h1>Sign Up!</h1>
                 <label for = "username"><strong>Username:</strong></label>
-                <input type = "text" size = "16" name = "username" placeholder="@Username"/></br>
+                <input type = "text" size = "16" name = "Username" placeholder="@Username"/></br>
                 <label for = "password"><strong>Password:</strong></label>
-                <input type = "password" size = "16" name = "password" placeholder="Password"/></br>
-                <label for = "Name"><strong>Name:</strong></label>
-                <input type = "text" size = "16" name = "name" placeholder="Name"/></br>
-                <input type = "submit" value = "Sign Up!"/>
+                <input type = "password" size = "16" name = "Password" placeholder="Password"/></br>
+                <label for = "name"><strong>Name:</strong></label>
+                <input type = "text" size = "16" name = "Name" placeholder="Name"/></br>
+                <input type = "submit" name ="Submit" value = "Sign Up!"/>
                 <p>Already have an account? <a href="index.php">Log in</a></p>
             </fieldset>
             
