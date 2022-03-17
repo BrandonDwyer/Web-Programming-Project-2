2063eb0b9ac93af2569d74be7e0c2b81e31d8a20
oxlngo <96035405+oxlngo@users.noreply.github.com>
2022-03-17 19:28:39 -0400

    last one

--- index.php
+++ index.php
@@ -3,21 +3,38 @@
   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
     <head>
-        <title>Jeopardy! - Sign in</title>
+        <title>Jeopardy!</title>
         <link type = "text/css" rel = "stylesheet" href = "style.css"/>
         <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
     </head>
     <body>
 
-       
-        <form action = "login-submit.php" method = "POST">
+    <?php
+            session_start();
+        ?>
+
+        <?php
+            if(isset($_SESSION['loginFail'])){
+                //Notification to the user that their Username or Password is not correct. 
+                //The flag is checked and unset so future login attempts/signins do not show this message if the error is repeated.
+                if($_SESSION['loginFail'] == TRUE){
+                    echo "<p>Incorrect username or password, please try again.<p>";
+                    $_SESSION['loginFail'] = FALSE;
+                }
+            }
+        ?>
+        
+
+
+
+<form action = "login-submit.php" method = "POST">
             <fieldset>
                 <h1>Sign in!</h1>
                 <label for = "username"><strong>Username:</strong></label>
-                <input type = "text" placeholder="@UserName" size = "16" name = "username" required></br>
+                <input type = "text" placeholder="@UserName" size = "16" name = "Username" required></br>
                 <label for = "password"><strong>Password:</strong></label>
-                <input type = "password" placeholder="Password" size = "16" name = "password" required></br>
-                <input type = "submit" value = "Login"/>
+                <input type = "password" placeholder="Password" size = "16" name = "Password" required></br>
+                <input type = "submit" name="Submit" value = "Login"/>
                 <p>Don't have an account? <a href = "signup.php">Sign up</a></p>
             </fieldset>
             
