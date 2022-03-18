a13753a25dbfb869f6486f9a4763f68a3e5cb000
aahmedyusuf <aahmedyusuf2@student.gsu.edu>
2022-03-17 23:39:33 -0400

    reworked on the GameScreen and Gameplay

---
+++ GameScreen.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="jeopardy.css">
+<title> Jeporady Main Page</title>
+</head>
+<body>
+
+<h1> JEOPARDY !</h1>
+
+<!-- Creating Game Board Table -->
+<table>
+
+<!-- Creating Category Row For Game Board -->
+<tr>
+<td> Sports</td> 
+<td> History</td> 
+<td> Science</td> 
+<td> Geography</td> 
+<td> Cars</td> 
+<td> Animals</td>
+</tr>
+
+<!-- Creating $200 Value Row For Game Board -->
+<tr>
+<td> <a href=QuestionScreen.php?1> $200</a> </td> 
+
+<td> <a href="/jeopardyQuestion/historyQuestion1.php"> $200</a> </td> 
+
+<td> <a href="/jeopardyQuestion/scienceQuestions1.php"> $200</a> </td> 
+
+<td> <a href="/jeopardyQuestion/geographyQuestion1.php"> $200</a> </td> 
+
+<td> <a href="/jeopardyQuestion/carsQuestion1.php"> $200</a> </td> 
+
+<td> <a href="/jeopardyQuestion/animalsQuestion1.php"> $200</a> </td>
+</tr>
+
+</table>
+<!-- End of Game Board Table-->
+
+
+</body>
+</html>
---
+++ QuestionScreen.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+<link rel="stylesheet" href="../jeopardy.css">
+<title> Jeporady Question Page</title>
+</head>
+<body>
+
+<?php 
+
+    $data = array(
+        "1",
+        "Question HERE",
+        "Ans1",
+        "Ans 2",
+        "Ans 3",
+        "Ans 4",
+        "Ans 1",
+
+   );
+   $parameter = $_SERVER['QUERY_STRING'];
+
+   $txt_file  = file_get_contents('questions.txt');
+    $rows = explode("\n", $txt_file);
+    foreach($rows as $row => $data)
+    {
+        $row_data = explode(',', $data);
+        if($row_data[0] == $parameter){
+            $data = $row_data;
+        }
+    }
+   $selected = -1;
+
+   $answer = $_POST['ans'];  
+   if ($answer == $data[6]) {          
+       echo 'Correct';      
+   }
+   else {
+       echo 'Wrong';
+   } 
+
+?>
+<h1> JEOPARDY !</h1>
+
+
+
+
+<div class="submittion">
+<h1> <?php echo $data[1]; ?></h1>
+<div class = "Answers">
+<form method="post" >
+  <h2> <?php echo $data[2] ?> <input type="radio" name="ans" value="1"  /> </h2>
+  <h2> <?php echo $data[3] ?> <input type="radio" name="ans" value="2"  /> </h2>
+  <h2> <?php echo $data[4] ?><input type="radio" name="ans" value="3"  /> </h2>
+  <h2> <?php echo $data[5] ?> <input type="radio" name="ans" value="4"? /> </h2>
+  </div>
+  <input type="submit" value="submit" class = "submittionButton"/>
+
+</form>
+</div>
+
+
+
+<!-- End of Table-->
+
+
+</body>
+</html>
