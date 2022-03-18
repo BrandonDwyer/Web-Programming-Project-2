6f35660ba9428447806bc02871911d93e7bbd9ac
BrandonDwyer <77303306+BrandonDwyer@users.noreply.github.com>
2022-03-17 23:06:05 -0400

    added leader board page

--- login-submit.php
+++ login-submit.php
@@ -39,7 +39,7 @@
     }
     else{
         $_SESSION["USER"] = $currentUser;
-        header("Location: main.php");
+        header("Location: jeopardyHomepage.php");
     }
         ?>
     </body>
